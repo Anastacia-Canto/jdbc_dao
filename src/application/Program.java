53c2634fa0c56e597c72f9c172705a311a1bdd94
Anastacia <anaanestesia@gmail.com>
2023-08-24 00:11:15 +0100

    implementing findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department dep = new Department(1, "books");
-		System.out.println(dep);
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, dep);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
